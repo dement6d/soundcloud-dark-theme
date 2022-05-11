eb4ffbfa107c60ba36d7d06643a8874a83f36027
dement6d <cuff.ho@gmail.com>
2022-05-11 22:12:27 +0200

    blcok cookie popup

--- replaceLogo.js
+++ replaceLogo.js
@@ -1,4 +1,12 @@
 try {
     document.querySelector('header[role="banner"]').classList.remove('peace-logo')
+
+    if (document.URL.includes("soundcloud.com")) {
+        setTimeout(() => {
+            const popup = document.querySelector('#onetrust-consent-sdk');
+            if (popup) popup.remove();
+            return 0;
+        }, 1000)
+    }
 }
 catch {}
