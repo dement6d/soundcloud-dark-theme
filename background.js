bdf510eecd2c8d67da76ee613586a0e130ca149a
dement6d <cuff.ho@gmail.com>
2022-05-11 18:17:33 +0200

    fix playcontorls buttons and add hover change

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.runtime.onInstalled.addListener(async () => {
     let url = "https://paypal.me/dement6d"
-    await chrome.tabs.create({ url });
+    //await chrome.tabs.create({ url });
 })
